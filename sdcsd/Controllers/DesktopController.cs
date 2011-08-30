1cd383002563a0da523ae06ef2e604481b70fa49
Petri Tuononen <petri.tuononen@gmail.com>
2011-08-30 09:39:17 +0000

    Desktop related files.

---
+++ sdcsd/Controllers/DesktopController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sdcsd.Models;
+
+namespace sdcsd.Controllers
+{
+    public class DesktopController : Controller
+    {
+        //
+        // GET: /Desktop/
+
+        public ActionResult Desktop()
+        {
+            var model = new DesktopModel()
+            {
+                Name = "Minutes2010.txt",
+                Icon = "txt.png",
+                Type = "file",
+                LocX = 10,
+                LocY = 10
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ sdcsd/Models/DesktopModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sdcsd.Models
+{
+    public class DesktopModel
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Icon { get; set; }
+        public String Type { get; set; }
+        public int LocX { get; set; }
+        public int LocY { get; set; }
+        //public bool Hide { get; set; }
+    }
+}
