c40b7c9e306d7b41284c45d277c73316a43968b1
Ari Rokosa <ari.rokosa@utu.fi>
2011-08-30 09:19:17 +0000

    Added model, views and controller for Topic feature (Ari)

---
+++ sdcsd/Controllers/TopicsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using sdcsd.Models;
+
+namespace sdcsd.Controllers
+{ 
+    public class TopicsController : Controller
+    {
+        private TopicDBContext db = new TopicDBContext();
+
+        //
+        // GET: /Topics/
+
+        public ViewResult Index()
+        {
+            return View(db.Topics.ToList());
+        }
+
+        //
+        // GET: /Topics/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Topic topic = db.Topics.Find(id);
+            return View(topic);
+        }
+
+        //
+        // GET: /Topics/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Topics/Create
+
+        [HttpPost]
+        public ActionResult Create(Topic topic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Topics.Add(topic);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(topic);
+        }
+        
+        //
+        // GET: /Topics/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Topic topic = db.Topics.Find(id);
+            return View(topic);
+        }
+
+        //
+        // POST: /Topics/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Topic topic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(topic).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(topic);
+        }
+
+        //
+        // GET: /Topics/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Topic topic = db.Topics.Find(id);
+            return View(topic);
+        }
+
+        //
+        // POST: /Topics/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Topic topic = db.Topics.Find(id);
+            db.Topics.Remove(topic);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ sdcsd/Models/Topic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace sdcsd.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class TopicDBContext : DbContext
+    {
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
