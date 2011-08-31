5979fae255576a519b8121a2a9a225fa8bdac0db
Petri Tuononen <petri.tuononen@gmail.com>
2011-08-31 11:45:51 +0000

    DesktopModel added.

---
+++ sdcsd/Models/DesktopModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sdcsd.Models
+{
+    public class DesktopModel
+    {
+        public int DesktopID { get; set; }
+    }
+}
