3d1aac1f0077b45b85acd3493042ac9f8fdde3bc
Petri Tuononen <petri.tuononen@gmail.com>
2011-09-01 17:22:20 +0000

    Add a file dialog saves a file to ~/Content/desktopFiles

--- sdcsd/Controllers/DesktopController.cs
+++ sdcsd/Controllers/DesktopController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using sdcsd.Models;
+using System.IO;
 
 namespace sdcsd.Controllers
 {
@@ -39,15 +40,38 @@ namespace sdcsd.Controllers
         }
 
         [HttpPost]
-        public JsonResult Upload(int? id, string qqfile)
+        public ActionResult Upload(string qqfile)
         {
-            var data = new
+            var path = Server.MapPath("~/Content/desktopFiles");
+            var file = string.Empty;
+
+            try
             {
-                success =  true,
-                complete = true
-            };
-            
-            return Json(data, JsonRequestBehavior.AllowGet);
+                var stream = Request.InputStream;
+                if (String.IsNullOrEmpty(Request["qqfile"]))
+                {
+                    // IE
+                    HttpPostedFileBase postedFile = Request.Files[0];
+                    stream = postedFile.InputStream;
+                    file = Path.Combine(path, System.IO.Path.GetFileName(Request.Files[0].FileName));
+                }
+                else
+                {
+                    //Webkit, Mozilla
+                    file = Path.Combine(path, qqfile);
+                }
+
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                System.IO.File.WriteAllBytes(file, buffer);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, "application/json");
+            }
+
+            return Json(new { success = true }, "text/html");
         }
+
     }
 }
